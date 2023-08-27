44537148dbfaa1e4f9a98e4b540cbb5e45e1ab9d
nikhilachalkar <142605075+nikhilachalkar@users.noreply.github.com>
2023-08-27 19:43:50 +0530

    Update 2ndserver.js

--- 2ndserver.js
+++ 2ndserver.js
@@ -1,45 +1,26 @@
-// const http = require('http');
-// const fs = require('fs');
-// const PORT = process.env.PORT || 3000;
-
-// const server = http.createServer((req, res) => {
-//     if (req.url === '/') {
-//         // Serve the HTML file
-//         fs.readFile('login.html', 'utf8', (err, data) => {
-//             if (err) {
-//                 res.writeHead(500, { 'Content-Type': 'text/plain' });
-//                 res.end('Internal Server Error');
-//             } else {
-//                 res.writeHead(200, { 'Content-Type': 'text/html' });
-//                 res.end(data);
-//             }
-//         });
-//     } else {
-//         // Handle other requests
-//         res.writeHead(404, { 'Content-Type': 'text/plain' });
-//         res.end('Not Found');
-//     }
-// });
-
-// server.listen(PORT, () => {
-//     console.log(`Server listening on port ${PORT}`);
-// });
-const express = require('express');
-const path = require('path');
-
-const app = express();
+const http = require('http');
+const fs = require('fs');
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from the "public" directory
-app.use(express.static(path.join(__dirname, 'public')));
-app.get('/', async(req, res) => {
-    res.sendFile(path.join(__dirname, 'public', '/login.html'));
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        // Serve the HTML file
+        fs.readFile('login.html', 'utf8', (err, data) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('Internal Server Error');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end(data);
+            }
+        });
+    } else {
+        // Handle other requests
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Not Found');
+    }
 });
 
-
-// Start the server
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 });
-
-
