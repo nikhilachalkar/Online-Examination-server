ec9869b382496e27d4e871bbf05b1d5c743f5f9c
nikhilachalkar <142605075+nikhilachalkar@users.noreply.github.com>
2023-08-27 13:11:22 +0530

    Add files via upload

---
+++ 2ndserver.js
@@ -0,0 +1,26 @@
+const http = require('http');
+const fs = require('fs');
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        // Serve the HTML file
+        fs.readFile('login.html', 'utf8', (err, data) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('Internal Server Error');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end(data);
+            }
+        });
+    } else {
+        // Handle other requests
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Not Found');
+    }
+});
+
+server.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
